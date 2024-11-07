d942d7c675a4909ce0d9ee03bcdb175a24f80b63
Narashiman Krishnamurthy <167101840+Narashiman-K@users.noreply.github.com>
2024-11-07 11:00:43 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import copy from "rollup-plugin-copy";
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    //__APP_ENV__: process.env.VITE_VERCEL_ENV,
     __APP_ENV__: process.env.APP_ENV,
   },
   plugins: [
